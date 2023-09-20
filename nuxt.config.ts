d38a4e632033f26178a858d8d3607f474c5a9bbc
Akayy <techakayy@gmail.com>
2023-09-20 20:34:11 +1000

    Used Pooya's suggestion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,8 @@
-import {fileURLToPath, URL} from 'node:url'
-import chokidar from 'chokidar'
-let pluginFileWatcher
-
 export default defineNuxtConfig({
-  modules: [
-    function (inlineOptions, nuxt) {
-      nuxt.hook('listen', async (listenerServer) => {
-        // @ts-ignore
-        const port = listenerServer.address().port
-        console.log(`Devserver running at: ${port}`)
-      })
+  hooks: {
+    listen: async (server, listener) => {
+      const urls = await listener.getURLs()
+      console.log(urls)
     },
-  ],
-  devtools: {enabled: true},
+  },
 })
-
-// function (inlineOptions, nuxt) {
-//   if (pluginFileWatcher) {
-//     pluginFileWatcher.close()
-//     pluginFileWatcher = null
-//   }
-//   if (!pluginFileWatcher) {
-//     pluginFileWatcher = chokidar.watch(
-//       fileURLToPath(new URL('./my-config.ts', import.meta.url)),
-//       {
-//         ignoreInitial: true,
-//       }
-//     )
-
-//     pluginFileWatcher
-//       .on('change', (filePath) => {
-//         nuxt.callHook('restart', {hard: true})
-//       })
-//       .on('error', (error) => {
-//         console.log(error)
-//         console.log(
-//           `Pinegrow: An error occured. Try re-starting your dev-server!`
-//         )
-//       })
-//   }
-
-//   nuxt.hook('ready', async (nuxt) => {
-//     console.log('Nuxt is ready!')
-//   })
-// },
